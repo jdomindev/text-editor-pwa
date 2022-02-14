e7f1c4eeeecf3e6d12b998b9e763c8e2a22c63b9
Jose Dominguez <jdomindev@gmail.com>
2022-02-14 16:04:32 -0500

    commented out the console logs

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,18 +14,18 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log("PUT to the database");
+  // console.log("PUT to the database");
   const jateDb = await openDB("jate", 1);
   const tx = jateDb.transaction("jate", "readwrite");
   const store = tx.objectStore("jate");
   const request = store.put({ id: 1, content: content });
   const result = await request;
-  console.log("🚀 - data saved to the database", result);
+  // console.log("🚀 - data saved to the database", result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.log("GET from the database");
+  // console.log("GET from the database");
 
   // Create a connection to the database database and version we want to use.
   const jateDb = await openDB("jate", 1);
